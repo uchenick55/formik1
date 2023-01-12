bbbde25ff19c3c6dea56a4012081ea9b04a423d6
uchenick55 <uchenick55@yandex.ru>
2023-01-12 22:51:49 +0300

    фух добил демонстрационный код на Tutorial

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,50 +1,78 @@
 import React from "react";
-import {Formik, Field, Form, ErrorMessage} from "formik";
+import {Formik, Form, useField} from "formik";
 import * as Yup from 'yup'
 
-/*
-const validate = values => {
-    const errors = {}
-
-    if (!values.firstName) {
-        errors.firstName = 'Required'
-    } else if (values.firstName.length>15) {
-        errors.firstName = 'Must be 15 characters or less'
-    }
-
-    if (!values.lastName) {
-        errors.lastName = 'Required'
-    } else if (values.lastName.length>20) {
-        errors.lastName = 'Must be 20 characters or less'
-    }
-
-    if (!values.email) {
-        errors.email = 'Required'
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
-        errors.email = 'Invalid email address'
-    }
-    return errors
+const MyTextInput = ({label, ...props}) => {
+    const [field, meta] = useField(props)
+    return (
+        <>
+            <label htmlFor={props.id || props.name}>{label}</label>
+            <input className='text-input' {...field} {...props}/>
+            {meta.touched && meta.error && <div className='error'>{meta.error}</div> }
+            <br/>
+        </>
+    )
 }
-*/
+
+const MyCheckbox = ({children, ...props}) => {
+    const [field, meta] = useField({...props, type: 'checkbox'})
+    return (
+        <div>
+            <label className='checkbox-input'>
+                <input type='checkbox' {...field} {...props}/>
+                {children}
+            </label>
+            {meta.touched && meta.error && <div className='error'>{meta.error}</div>}
+            <br/>
+        </div>
+    )
+}
+
+const MySelect = ({label, ...props}) => {
+    const [field, meta] = useField(props)
+    return (
+        <div>
+            <label htmlFor={props.id || props.name}>{label}</label>
+            <select {...field} {...props}/>
+            {meta.touched && meta.error && <div className='error'>{meta.error}</div>}
+            <br/>
+        </div>
+    )
+
+}
+
 
 const SignupForm = ({onSubmitForm}) => {
     return (
+        <>
+        <h1>Subscribe!</h1>
         <Formik
             initialValues={{
                 firstName: "",
                 lastName: '',
                 email: "",
+                acceptedTerms: false, // добавлен для нашего чекбокса
+                jobType: "", // добавлен для нашего select
             }}
             validationSchema={Yup.object({
-                    firstName: Yup.string()
-                        .max(15, "Must be 15 character or less")
-                        .required('Required'),
-                    lastName: Yup.string()
-                        .max(20, "Must be 20 character or less")
-                        .required('Required'),
-                    email: Yup.string()
-                        .email('Invalid email address')
-                        .required('Required'),
+                firstName: Yup.string()
+                    .max(15, "Must be 15 character or less")
+                    .required('Required'),
+                lastName: Yup.string()
+                    .max(20, "Must be 20 character or less")
+                    .required('Required'),
+                email: Yup.string()
+                    .email('Invalid email address')
+                    .required('Required'),
+                acceptedTerms: Yup.boolean()
+                    .required('Required')
+                    .oneOf([true], 'You must accept the terms and conditions.'),
+                jobType: Yup.string()
+                    .oneOf(
+                        ['designer', 'development', 'product', 'other'],
+                        'Invalid Job Type'
+                    )
+                    .required('Required'),
                 }
             )}
             onSubmit={(values, {setSubmitting}) => {
@@ -55,36 +83,44 @@ const SignupForm = ({onSubmitForm}) => {
             }}
         >
             <Form>
-                <label htmlFor="firstName">First Name: </label>
-                <Field
-                    name="firstName"
+                <MyTextInput
+                    label="First Name"
+                    name = 'firstName'
                     type='text'
+                    placeholder='Jane'
                 />
-                <ErrorMessage name='firstName'/>
-                <br/>
 
-                <label htmlFor="lastName">Last Name: </label>
-                <Field
-                    name="lastName"
+                <MyTextInput
+                    label="Last Name"
+                    name = 'lastName'
                     type='text'
+                    placeholder='Doe'
                 />
-                <ErrorMessage name='lastName'/>
-                <br/>
 
-                <label htmlFor="email">Email Address: </label>
-                <Field
-                    name="email"
+                <MyTextInput
+                    label="Email Address"
+                    name = 'email'
                     type='email'
+                    placeholder='jane@formik.com'
                 />
-                <ErrorMessage name='email'/>
-                <br/>
+
+                <MySelect label='Job Type' name = 'jobType'>
+                    <option value=''>Select a job type</option>
+                    <option value='designer'>Designer</option>
+                    <option value='development'>Developer</option>
+                    <option value='product'>Product Manager</option>
+                    <option value='other'>Other</option>
+                </MySelect>
+
+                <MyCheckbox name='acceptedTerms'>
+                    I accept the terms and conditions
+                </MyCheckbox>
 
                 <button type='submit'>Submit</button>
-                <br/>
             </Form>
         </Formik>
+        </>
     )
 }
-
 
 export default SignupForm
