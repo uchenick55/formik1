a91c33ab097a6e0eb422ce3e2384f398878cf4a8
uchenick55 <uchenick55@yandex.ru>
2023-01-12 21:54:03 +0300

    <Form>,  <Field/>, <ErrorMessage/>

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Formik} from "formik";
+import {Formik, Field, Form, ErrorMessage} from "formik";
 import * as Yup from 'yup'
 
 /*
@@ -54,39 +54,34 @@ const SignupForm = ({onSubmitForm}) => {
                 }, 400)
             }}
         >
-            {formik => (
-                <form onSubmit={formik.handleSubmit}>
-                    <label htmlFor="firstName">First Name: </label>
-                    <input
-                        id="firstName"
-                        type='text'
-                        {...formik.getFieldProps('firstName')}
-                    />
-                    {formik.touched.firstName && formik.errors.firstName && <div>{formik.errors.firstName}</div>}
-                    <br/>
+            <Form>
+                <label htmlFor="firstName">First Name: </label>
+                <Field
+                    name="firstName"
+                    type='text'
+                />
+                <ErrorMessage name='firstName'/>
+                <br/>
 
-                    <label htmlFor="lastName">Last Name: </label>
-                    <input
-                        id="lastName"
-                        type='text'
-                        {...formik.getFieldProps('lastName')}
-                    />
-                    {formik.touched.lastName && formik.errors.lastName && <div>{formik.errors.lastName}</div>}
-                    <br/>
+                <label htmlFor="lastName">Last Name: </label>
+                <Field
+                    name="lastName"
+                    type='text'
+                />
+                <ErrorMessage name='lastName'/>
+                <br/>
 
-                    <label htmlFor="email">Email Address: </label>
-                    <input
-                        id="email"
-                        type='email'
-                        {...formik.getFieldProps('email')}
-                    />
-                    {formik.touched.email && formik.errors.email && <div>{formik.errors.email}</div>}
-                    <br/>
+                <label htmlFor="email">Email Address: </label>
+                <Field
+                    name="email"
+                    type='email'
+                />
+                <ErrorMessage name='email'/>
+                <br/>
 
-                    <button type='submit'>Submit</button>
-                    <br/>
-                </form>
-            )}
+                <button type='submit'>Submit</button>
+                <br/>
+            </Form>
         </Formik>
     )
 }
