75e6ea280a5b999a2368119d857fdadad78b8084
uchenick55 <uchenick55@yandex.ru>
2023-01-13 11:26:22 +0300

    2 варианта ввода плюс композиция input/textarea

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,16 +1,35 @@
 import React from "react";
-import {Formik, Form, useField} from "formik";
-import * as Yup from 'yup'
+import {Formik, Form, useField, Field, ErrorMessage} from "formik"; //формик с компонентами и пользовательским хуком
+import * as Yup from 'yup' // валидация форм с помошью сторонней библиотеки Yup
 
-const MyTextInput = ({label, ...props}) => {
-    const [field, meta] = useField(props)
+
+const Composition = ({label, children, ...props}) => {
+    const [field, meta] = useField(props.props) // получить доп данные (blur/error)
     return (
         <>
-            <label htmlFor={props.id || props.name}>{label}</label>
-            <input className='text-input' {...field} {...props}/>
-            {meta.touched && meta.error && <div className='error'>{meta.error}</div> }
-            <br/>
+            <label htmlFor={props.props.id || props.props.name}>{label}</label> {/*лейб либо с props.id (которого пока нет), либо с props.name*/}
+            <br/> {/*перенос стоки*/}
+            {children}
+            {meta.touched && meta.error && <div className='error'>{meta.error}</div>} {/*вывод ошибок если касались поля и есть ошибка*/}
+            <br/>{/*перенос стоки*/}
         </>
+    )
+}
+
+const MyTextArea = ({label, ...props}) => {  // вынесенная общая часть для тесктового поля
+    const [field] = useField(props) // получить доп данные (blur/error)
+    return (
+        <Composition label={label} props={props}>
+            <textarea className='text-area' {...field} {...props}/>  {/*то различие между input и textarea*/}
+        </Composition>
+    )
+}
+const MyTextInput = ({label, ...props}) => {  // вынесенная общая часть для тесктового поля
+    const [field, meta] = useField(props)
+    return (
+        <Composition label={label} props={props}>
+            <input className='text-input' {...field} {...props}/> {/*то различие между input и textarea*/}
+        </Composition>
     )
 }
 
@@ -38,89 +57,130 @@ const MySelect = ({label, ...props}) => {
             <br/>
         </div>
     )
-
 }
-
 
 const SignupForm = ({onSubmitForm}) => {
     return (
         <>
-        <h1>Subscribe!</h1>
-        <Formik
-            initialValues={{
-                firstName: "",
-                lastName: '',
-                email: "",
-                acceptedTerms: false, // добавлен для нашего чекбокса
-                jobType: "", // добавлен для нашего select
-            }}
-            validationSchema={Yup.object({
-                firstName: Yup.string()
-                    .max(15, "Must be 15 character or less")
-                    .required('Required'),
-                lastName: Yup.string()
-                    .max(20, "Must be 20 character or less")
-                    .required('Required'),
-                email: Yup.string()
-                    .email('Invalid email address')
-                    .required('Required'),
-                acceptedTerms: Yup.boolean()
-                    .required('Required')
-                    .oneOf([true], 'You must accept the terms and conditions.'),
-                jobType: Yup.string()
-                    .oneOf(
-                        ['designer', 'development', 'product', 'other'],
-                        'Invalid Job Type'
-                    )
-                    .required('Required'),
-                }
-            )}
-            onSubmit={(values, {setSubmitting}) => {
-                setTimeout(() => {
-                    alert(JSON.stringify(values, null, 2));
-                    onSubmitForm(values)
-                }, 400)
-            }}
-        >
-            <Form>
-                <MyTextInput
-                    label="First Name"
-                    name = 'firstName'
-                    type='text'
-                    placeholder='Jane'
-                />
+            <h1>Subscribe!</h1>
+            <Formik
+                initialValues={{
+                    firstName: "firstName",
+                    lastName: 'lastName',
+                    email: "email@email.em",
+                    acceptedTerms: true, // добавлен для нашего чекбокса
+                    jobType: "product", // добавлен для нашего select
+                    message: "Textarea Field", //
+                    comments: "MyTextArea",
+                    picked: "Two"
+                }}
+                validationSchema={Yup.object({
+                        firstName: Yup.string()
+                            .max(15, "Must be 15 character or less")
+                            .required('Required'),
+                        lastName: Yup.string()
+                            .max(20, "Must be 20 character or less")
+                            .required('Required'),
+                        email: Yup.string()
+                            .email('Invalid email address')
+                            .required('Required'),
+                        comments: Yup.string()
+                            .max(50, 'Must be 50 characters or less')
+                            .required('Required'),
+                        acceptedTerms: Yup.boolean()
+                            .required('Required')
+                            .oneOf([true], 'You must accept the terms and conditions.'),
+                        jobType: Yup.string()
+                            .oneOf(
+                                ['designer', 'development', 'product', 'other'],
+                                'Invalid Job Type'
+                            )
+                            .required('Required'),
+                    }
+                )}
+                onSubmit={(values, {setSubmitting}) => {
+                    setTimeout(() => {
+                        alert(JSON.stringify(values, null, 2)); // после отправки формы вывести alert
+                        onSubmitForm(values) // колбек, который принмает результат ввода формы
+                    }, 400)
+                }}
+            >
+                {({values}) => ( // обертка для вывода значений ввода в любом месте формы паралельно (или в итоге)
 
-                <MyTextInput
-                    label="Last Name"
-                    name = 'lastName'
-                    type='text'
-                    placeholder='Doe'
-                />
+                    <Form>
 
-                <MyTextInput
-                    label="Email Address"
-                    name = 'email'
-                    type='email'
-                    placeholder='jane@formik.com'
-                />
+                        <label htmlFor="firstName">First Name</label>
+                        <Field name="firstName" type="text"/>
+                        <ErrorMessage name="firstName"/>
 
-                <MySelect label='Job Type' name = 'jobType'>
-                    <option value=''>Select a job type</option>
-                    <option value='designer'>Designer</option>
-                    <option value='development'>Developer</option>
-                    <option value='product'>Product Manager</option>
-                    <option value='other'>Other</option>
-                </MySelect>
+                        <label htmlFor="firstName">Textarea Field</label>
+                        <Field name="message" as="textarea" className="form-textarea"/>
+                        <ErrorMessage name="message"/>
+                        <div>firstName: {values.message}</div>
 
-                <MyCheckbox name='acceptedTerms'>
-                    I accept the terms and conditions
-                </MyCheckbox>
 
-                <button type='submit'>Submit</button>
-            </Form>
-        </Formik>
+                        <div id="my-radio-group">Picked</div>
+                        <div role="group" aria-labelledby="my-radio-group">
+                            <label>
+                                <Field type="radio" name="picked" value="One"/>
+                                One
+                            </label>
+                            <label>
+                                <Field type="radio" name="picked" value="Two"/>
+                                Two
+                            </label>
+
+                            <div>Picked: {values.picked}</div>
+
+                        </div>
+
+                        <MyTextInput
+                            label="First Name"
+                            name='firstName'
+                            type='firstName'
+                            placeholder='Jane'
+                        />
+
+                        <MyTextInput
+                            label="Last Name"
+                            name='lastName'
+                            type='text'
+                            placeholder='Doe'
+                        />
+
+                        <MyTextInput
+                            label="Email Address"
+                            name='email'
+                            type='email'
+                            placeholder='jane@formik.com'
+                        />
+
+                        <MyTextArea
+                            label="MyTextArea"
+                            name='comments'
+                            type='text'
+                            placeholder='Your comments to Subscribe'
+                        />
+
+                        <MySelect label='Job Type' name='jobType'>
+                            <option value=''>Select a job type</option>
+                            <option value='designer'>Designer</option>
+                            <option value='development'>Developer</option>
+                            <option value='product'>Product Manager</option>
+                            <option value='other'>Other</option>
+                        </MySelect>
+
+                        <MyCheckbox name='acceptedTerms'>
+                            I accept the terms and conditions
+                        </MyCheckbox>
+
+                        <button type='submit'>Submit</button>
+                    </Form>
+                )}
+            </Formik>
         </>
     )
 }
 
 export default SignupForm
+
